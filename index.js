de628145c680b72f12ed010b42c70bfe845e63a4
Siddhant Prasad <siddhantprasad.tvnl@gmail.com>
2024-05-14 21:33:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//Basics
+//console.log("Jai Shree Ram");
+//console.log(age); no hoisting
+// let age: number = 10;
+// //age = 'a'; can't
+// if(age > 60){
+//     console.log("Retire");
+// }
+//Built-in types
+// let sales = 123;
+// console.log(sales);
+// let p = true; 
+// let m = 12_000_000;
+// console.log(m+sales);
+//any type
+// let l;
+// console.log(l);
+// function(l){
+// Parameter 'l' implicitly has an 'any' type
+//     console.log(l);
+// }
+//Arrays
+// let arr : number[] = []
+// arr.push(1);
+// arr.push(7); //thala for a reason
+// //arr.push('3') -> error dega
+// arr.forEach(element => {
+//     //element.toExponential(3)
+//     // element.toLocaleString();
+//     console.log(element);
+// });
+//console.log(arr);
+//Tuples
+var tup = [7, "Thala"];
+console.log(tup);
+tup[0] = 18;
+tup[1] = "Koach";
+console.log(tup);
